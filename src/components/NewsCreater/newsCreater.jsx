9b7ca452fe33ccf286dca9e0e2c90bc35679642f
Seraphic <danilsocol-2002@mail.ru>
2022-12-28 15:21:21 +0500

    final

--- src/components/NewsCreater/newsCreater.jsx
+++ src/components/NewsCreater/newsCreater.jsx
@@ -4,15 +4,25 @@ import closeBtn from '@Assets/images/closeBtn.jpg';
 import './newsCreater.less';
 
 const NewsCreater = () => {
-    const [img, setImg] = useState(false);
-    const [text, setText] = useState(false);
+    const [img, setImg] = useState(null);
+    const [text, setText] = useState(null);
+    const [errorText, setErrorText] = useState(true);
+    const [errorImg, setErrorImg] = useState(true);
+    const [count, setCount] = useState(0);
     function addImg(e) {
-        setImg(URL.createObjectURL(e.target.files[0]));
-        setText(e.target.files[0].name);
+        const validator = new RegExp('.*.(jpg|jpeg|png|gif|svg)');
+
+        if (!validator.test(e.target.files[0].name)) {
+            setErrorImg(false);
+        } else {
+            setImg(URL.createObjectURL(e.target.files[0]));
+            setText(e.target.files[0].name);
+            setErrorImg(true);
+        }
     }
     function deleteImg() {
-        setImg(false);
-        setText(false);
+        setImg(null);
+        setText(null);
     }
     return (
         <div className="comment-block">
@@ -21,7 +31,22 @@ const NewsCreater = () => {
                 className="message-box"
                 onSubmit={(e) => {
                     e.preventDefault();
-                    return false;
+
+                    if (!e.target[0].value.isEmpty) {
+                        setErrorText(false);
+                    } else {
+                        setErrorText(true);
+                    }
+
+                    if (!e.target[0].value.isEmpty) {
+                        setErrorImg(false);
+                    } else {
+                        setErrorImg(true);
+                    }
+
+                    if (!e.target[1].value && !e.target[0].value) return true;
+
+                    return true;
                 }}
             >
                 <div className="container-comment-title">
@@ -34,10 +59,30 @@ const NewsCreater = () => {
                     </span>
                 </div>
 
-                <textarea
-                    className="text-area"
-                    placeholder="Введите текст новости"
-                ></textarea>
+                <div
+                    className={
+                        errorText
+                            ? 'container-text-area'
+                            : 'container-text-area-red'
+                    }
+                >
+                    <textarea
+                        className="text-area"
+                        placeholder="Введите текст новости"
+                        onChange={(e) => setCount(e.target.value.length)}
+                    ></textarea>
+                    {count !== 0 || !errorText ? (
+                        <div className="limit-text">
+                            <span>Cимволов: </span>
+                            <span className={count > 150 ? 'error' : ''}>
+                                {count}
+                            </span>
+                            <span>/150</span>
+                        </div>
+                    ) : (
+                        <div></div>
+                    )}
+                </div>
 
                 <div className="container-add-img">
                     <label className="input-file">
@@ -45,21 +90,33 @@ const NewsCreater = () => {
                         <img className="add-img" src={imgAdd} />
                         <span className="add-text">+ Добавить изображение</span>
                     </label>
-
-                    <img
+                    <div className="container-added-img">
+                        <img
+                            className={
+                                img == null ? 'added-img-null' : 'added-img'
+                            }
+                            src={img}
+                        />
+                        <span className="added-text">{text} </span>
+                        <img
+                            src={closeBtn}
+                            className={
+                                img == null ? 'close-btn-null' : 'close-btn'
+                            }
+                            onClick={() => deleteImg()}
+                        />
+                    </div>
+                    <div
                         className={
-                            img == false ? 'added-img-null' : 'added-img'
+                            errorImg
+                                ? 'container-error-text-none'
+                                : 'container-error-text'
                         }
-                        src={img}
-                    />
-                    <span className="added-text">{text} </span>
-                    <img
-                        src={closeBtn}
-                        className={
-                            img == false ? 'close-btn-null' : 'close-btn'
-                        }
-                        onClick={() => deleteImg()}
-                    />
+                    >
+                        <span className="error">
+                            Неверный формат изображения
+                        </span>
+                    </div>
                 </div>
                 <button className="post-btn">Применить</button>
             </form>
